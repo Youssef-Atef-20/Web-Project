645d68d1bbe17f47b621aacb3e2242f8260a996a
Youssef Atef <hh3928850@gmail.com>
2026-05-02 04:49:44 +0300

    d

--- index.php
+++ index.php
@@ -159,8 +159,8 @@
     </footer>
 
     <!-- AOS JS -->
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js" defer></script>
     <!-- Custom JS -->
-    <script src="script.js"></script>
+    <script src="script.js" defer></script>
 </body>
 </html>
